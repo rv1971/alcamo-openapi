115c392bc8e3696f810e31049d40b74258138a34
rve <r.verhoeven@weat.de>
2023-02-20 21:49:20 +0100

    Fix class name of JsonDocumentFactory

--- src/JsonDocumentFactory.php
+++ src/JsonDocumentFactory.php
@@ -5,7 +5,7 @@ namespace alcamo\openapi;
 use alcamo\json\JsonDocumentFactory;
 
 /// Factory for documents made of OpenAPI nodes
-class DocumentFactory extends JsonDocumentFactory
+class JsonDocumentFactory extends JsonDocumentFactory
 {
     public const DOCUMENT_CLASS = Document::class;
 }
